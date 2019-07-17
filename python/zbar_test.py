54b5b510c47b81256aa3e817109de2d62bae303e
Aaron Leopold <aaronleopold1221@gmail.com>
2019-07-17 14:37:48 -0400

    removed unneccessary file

--- python/zbar_test.py
+++
@@ -1,6 +0,0 @@
-from pyzbar.pyzbar import decode
-from PIL import Image
-
-decoded = decode(Image.open("/Users/aaronleopold/Documents/data_science/DataMatrix-Reader/images/test/barcode.JPG"))
-data = str(decoded[0].data).replace("b\'", '').replace(' ', '_').replace('\'', '')
-print (data)
